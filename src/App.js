7c9edd7e0cedd53f967c1713b7843c1f5ff2834a
tesfaye robelle <tdebella91@gail.com>
2022-09-13 18:54:15 -0400

    about page added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Hero from './Components/Hero'
+import Hero from "./Components/Hero";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <AboutUs />
     </div>
   );
 }
-
 
 export default App;
