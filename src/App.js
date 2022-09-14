6409df98bde48ad152661d5a987471d609837462
tesfaye robelle <tdebella91@gail.com>
2022-09-14 13:31:45 -0400

    about component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import AboutUs from "./Components/AboutUs";
+import Bartenders from "./Components/Bartenders";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <AboutUs />
+      <Bartenders />
     </div>
   );
 }
