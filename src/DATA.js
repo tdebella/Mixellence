461aa29b5e0b650a001d26a9e1b2d38bc7f04abf
mikegv <xmik3yvx@live.com>
2022-09-16 14:40:58 -0700

    updated DATA

--- src/DATA.js
+++ src/DATA.js
@@ -1,29 +1,36 @@
 module.exports = {
-    about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde deleniti eveniet quisquam animi commodi saepe eius consectetur. Lorem ipsum dolor sit amet consectetur, adipisicing consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde delenitiquisquam animi commodi saepe eius consectetur. Lorem ipsum dolor!",
+    about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde deleniti eveniet quisquam animi commodi saepe eius consectetur. Lorem ipsum dolor sit amet consectetur, adipisicing consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde delenitiquisquam animi commodi saepe eius consectetur. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde deleniti eveniet quisquam animi commodi saepe eius consectetur. Lorem ipsum dolor sit amet consectetur, adipisicing consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde delenitiquisquam animi commodi saepe eius consectetur. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde deleniti eveniet quisquam animi commodi saepe eius consectetur. Lorem ipsum dolor sit amet consectetur, adipisicing consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde delenitiquisquam animi commodi saepe eius consectetur. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde deleniti eveniet quisquam animi commodi saepe eius consectetur. Lorem ipsum dolor sit amet consectetur, adipisicing consectetur adipisicing elit. Nam ad reprehenderit laborum adipisci voluptatem itaque inventore, dolor amet iusto minus rerum unde delenitiquisquam animi commodi saepe eius consectetur. Lorem ipsum dolor!",
     drinks: [
         {
             name: 'paloma',
-            imgUrl: 'drinks-paloma.jpeg',
+            imgUrl: '../assets/Photos/drinks-paloma.jpeg',
             info: "Main alcohol: Tequila Ingredients: 1 oz Cointreau, 1 oz Lime juice, 2oz Tequila Preparation: Rub the rim of the glass with the lime sliceto make the salt stick to it. Shake the other ingredients with ice then carefully pour into the glass. Garnish and serve over ice.Served: On the rocks; poured over ice Standard garnish: Lime slice,Salted rim Drinkware: Margarita glass"
         },
         {   
             name: 'margarita',
-            imgUrl: 'drinks-margarita.jpeg',
+            imgUrl: '../assets/Photos/drinks-margarita.jpeg',
             info: "Main alcohol: Tequila Ingredients: 1 oz Cointreau, 1 oz Lime juice, 2oz Tequila Preparation: Rub the rim of the glass with the lime sliceto make the salt stick to it. Shake the other ingredients with ice then carefully pour into the glass. Garnish and serve over ice.Served: On the rocks; poured over ice Standard garnish: Lime slice,Salted rim Drinkware: Margarita glass"
         },
         {
             name: 'michelada',
-            imgUrl: 'drinks-michelada.jpeg',
+            imgUrl: '../assets/Photos/drinks-michelada.jpeg',
             info: "Main alcohol: Tequila Ingredients: 1 oz Cointreau, 1 oz Lime juice, 2oz Tequila Preparation: Rub the rim of the glass with the lime sliceto make the salt stick to it. Shake the other ingredients with ice then carefully pour into the glass. Garnish and serve over ice.Served: On the rocks; poured over ice Standard garnish: Lime slice,Salted rim Drinkware: Margarita glass"
         }
     ],
     bartenders: [
         {
             name: 'Hannah Gonzalez',
+            image: '../assets/Photos/michael-dam-mEZ3PoFGs_k-unsplash.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus'
         },
         {
             name: 'Ava Maxwell',
+            image: '../assets/Photos/prince-akachi-J1OScm_uHUQ-unsplash.jpg',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus'
+        },
+        {
+            name: 'Jack Allen',
+            image: '../assets/Photos/austin-wade-X6Uj51n5CE8-unsplash.jpg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae mi consequat adipiscing phasellus'
         }
     ],
