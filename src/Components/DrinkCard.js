2b124bd86a9af33c77ed23439285bc303b78f7bd
mikegv <xmik3yvx@live.com>
2022-09-20 15:39:25 -0700

    added firebase configuration and changed Drinks component with updated data source

--- src/Components/DrinkCard.js
+++ src/Components/DrinkCard.js
@@ -5,7 +5,7 @@ const DrinkCard = ({ drink }) => {
   //flip is used to set if we see the front or back of the drink card
   const [flip, setFlip] = useState(false);
   
-  let image = drink.imgUrl.toString();
+  let image = drink.photo.toString();
   return (
     <div
       className={`card ${flip ? "flip" : ""}`}
@@ -20,7 +20,7 @@ const DrinkCard = ({ drink }) => {
       <div className="back">
         <h3 className="heading">{drink.name}</h3>
         <p className="bodyText">
-          {drink.info}
+          {drink.description}
         </p>
       </div>
     </div>
--- src/Components/Drinks.js
+++ src/Components/Drinks.js
@@ -2,27 +2,9 @@ import './drinks.css'
 import DrinkCard from './DrinkCard';
 
 const Drinks = ({drinks}) => {
-    const drink_info = [
-        {
-            name: 'paloma',
-            imgUrl: 'drinks-paloma.jpeg',
-            info: "Main alcohol: Tequila Ingredients: 1 oz Cointreau, 1 oz Lime juice, 2oz Tequila Preparation: Rub the rim of the glass with the lime sliceto make the salt stick to it. Shake the other ingredients with ice then carefully pour into the glass. Garnish and serve over ice.Served: On the rocks; poured over ice Standard garnish: Lime slice,Salted rim Drinkware: Margarita glass"
-        },
-        {   
-            name: 'margarita',
-            imgUrl: 'drinks-margarita.jpeg',
-            info: "Main alcohol: Tequila Ingredients: 1 oz Cointreau, 1 oz Lime juice, 2oz Tequila Preparation: Rub the rim of the glass with the lime sliceto make the salt stick to it. Shake the other ingredients with ice then carefully pour into the glass. Garnish and serve over ice.Served: On the rocks; poured over ice Standard garnish: Lime slice,Salted rim Drinkware: Margarita glass"
-        },
-     
-        {
-            name: 'michelada',
-            imgUrl: 'drinks-michelada.jpeg',
-            info: "Main alcohol: Tequila Ingredients: 1 oz Cointreau, 1 oz Lime juice, 2oz Tequila Preparation: Rub the rim of the glass with the lime sliceto make the salt stick to it. Shake the other ingredients with ice then carefully pour into the glass. Garnish and serve over ice.Served: On the rocks; poured over ice Standard garnish: Lime slice,Salted rim Drinkware: Margarita glass"
-        }
-    ]
+  
     return (
         <div className='drinks'>
-           {console.log('drinks ', drinks)}
             <h2 className='heading sectionTitle'>Drinks</h2>
             <div className='cardContainer'>
             {
