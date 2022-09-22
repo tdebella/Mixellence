bb4b986f0b177c3c6953eb6fe12a0b24011200a4
tesfaye robelle <tdebella91@gail.com>
2022-09-22 18:29:55 -0400

    css code added

--- src/Components/Bartenders.js
+++ src/Components/Bartenders.js
@@ -1,13 +1,20 @@
-import Bartender from "./Bartender";
 import BartenderRight from "./BartenderRight";
+import BartenderLeft from "./BartenderLeft";
 
 const Bartenders = (props) => {
+  let left = true;
   return (
     <>
       <h1 className="title">GET TO KNOW YOUR BARTENDERS</h1>
-      {props.bartenders.map((bartender) => (
-        <BartenderRight bartender={bartender} />
-      ))}
+      {props.bartenders.map((bartender) => {
+        if (left) {
+          left = !left;
+          return <BartenderLeft bartender={bartender} />;
+        } else {
+          left = !left;
+          return <BartenderRight bartender={bartender} />;
+        }
+      })}
     </>
   );
 };
