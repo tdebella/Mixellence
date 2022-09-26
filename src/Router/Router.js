b8e01f18e06e94a7656a52d04ea6c65969ce6f4f
Mike <mikegv@users.noreply.github.com>
2022-09-26 14:12:21 -0700

    Mv dev (#26)
    * moved firebase logic out of app js

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../Components/Login/Login";
 import PasswordReset from "../Components/Login/PasswordReset";
+import MainPage from '../Components/MainPage'
 
 const Router = () => {
   return (
     <div>
       <Routes>
+        <Route path='/' exact element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/passwordReset" element={<PasswordReset />} />
       </Routes>
