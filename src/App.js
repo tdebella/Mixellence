d67630ecb585813bbe16732c3fff0b23eb918a2d
mikegv <xmik3yvx@live.com>
2022-09-26 14:11:09 -0700

    moved firebase logic out of app js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,11 @@
+import Router from "./Router/Router";
 import "./App.css";
-import { getDocs } from "firebase/firestore";
-import { aboutRef, barRef, drinksRef, testRef } from "./firebase-db";
-import { useEffect, useState } from "react";
-import AddItem from "./Components/dashboard/AddItem";
-import { ModalBackdrop } from "./styled-components/styled-components";
-import MainPage from "./Components/MainPage";
 
 function App() {
-  const [aboutUs, setAboutUs] = useState("");
-  const [bartenders, setBartenders] = useState([]);
-  const [drinks, setDrinks] = useState([]);
-  const [testimonials, setTestimonials] = useState([]);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    //get all the content from firestore database for each section
-    let aboutContent = [];
-
-    getDocs(aboutRef).then((res) => {
-      res.docs.forEach((docs) => {
-        aboutContent.push({ ...docs.data() });
-      });
-      setAboutUs(aboutContent[0].summary);
-    });
-
-    let bartenderContent = [];
-
-    getDocs(barRef).then((res) => {
-      res.docs.forEach((item) => {
-        bartenderContent.push({ ...item.data(), id: item.id });
-      });
-      setBartenders(bartenderContent);
-    });
-
-    let drinksContent = [];
-    getDocs(drinksRef).then((res) => {
-      res.docs.forEach((item) => {
-        drinksContent.push({ ...item.data(), id: item.id });
-      });
-      setDrinks(drinksContent);
-    });
-    let testimonialsContent = [];
-    getDocs(testRef).then((res) => {
-      res.docs.forEach((item) => {
-        testimonialsContent.push({ ...item.data(), id: item.id });
-      });
-      setTestimonials(testimonialsContent);
-    });
-  }, []);
 
   return (
     <div className="App">
-      <MainPage
-        bartenders={bartenders}
-        aboutUs={aboutUs}
-        drinks={drinks}
-        testimonials={testimonials}
-      />
+      <Router />
     </div>
   );
 }
--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Router from "../Router/Router";
+import { useEffect, useState } from "react";
+import { getDocs } from "firebase/firestore";
+import { aboutRef, barRef, drinksRef, testRef } from "../firebase-db";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
 import AboutUs from "../Components/AboutUs";
@@ -8,12 +10,54 @@ import Bartenders from "../Components/Bartenders";
 import Testimonials from "../Components/Testimonials";
 import Footer from "../Components/Footer";
 
-const MainPage = ({ aboutUs, drinks, bartenders, testimonials }) => {
+const MainPage = () => {
+
+  const [aboutUs, setAboutUs] = useState("");
+  const [bartenders, setBartenders] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    //get all the content from firestore database for each section
+    let aboutContent = [];
+
+    getDocs(aboutRef).then((res) => {
+      res.docs.forEach((docs) => {
+        aboutContent.push({ ...docs.data() });
+      });
+      setAboutUs(aboutContent[0].summary);
+    });
+
+    let bartenderContent = [];
+
+    getDocs(barRef).then((res) => {
+      res.docs.forEach((item) => {
+        bartenderContent.push({ ...item.data(), id: item.id });
+      });
+      setBartenders(bartenderContent);
+    });
+
+    let drinksContent = [];
+    getDocs(drinksRef).then((res) => {
+      res.docs.forEach((item) => {
+        drinksContent.push({ ...item.data(), id: item.id });
+      });
+      setDrinks(drinksContent);
+    });
+    let testimonialsContent = [];
+    getDocs(testRef).then((res) => {
+      res.docs.forEach((item) => {
+        testimonialsContent.push({ ...item.data(), id: item.id });
+      });
+      setTestimonials(testimonialsContent);
+    });
+  }, []);
+
   return (
     <>
       <Navbar />
       <Hero />
-      <Router />
       <AboutUs aboutUS={aboutUs} />
       <Drinks drinks={drinks} />
       <Bartenders bartenders={bartenders} />
