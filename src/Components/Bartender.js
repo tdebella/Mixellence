e49c6c41ec0ccead0ac406fdc58535514fe10254
tesfaye robelle <tdebella91@gail.com>
2022-09-26 11:38:35 -0400

    change regular css to custom css

--- src/Components/Bartender.js
+++ src/Components/Bartender.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "./Bartender.css";
+import classes from "./Bartender.module.css";
 import { StyledButton } from "../styled-components/styled-components";
 
 const Bartender = (props) => {
   return (
-    <div className="bartenderWrapper">
-      <h2 className="img">{props.bartender.photo}</h2>
-      <h3 className="subTitle">{props.bartender.name}</h3>
-      <p className="text">{props.bartender.summary}</p>
+    <div className={classes.bartenderWrapper}>
+      <h2 className={classes.img}>{props.bartender.photo}</h2>
+      <h3 className={classes.subTitle}>{props.bartender.name}</h3>
+      <p className={classes.text}>{props.bartender.summary}</p>
       <StyledButton>{"Book Now"}</StyledButton>
     </div>
   );
