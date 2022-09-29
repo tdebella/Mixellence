c6c971e63104e8b8fa0ad604c6b4fba32124a9de
Mike <mikegv@users.noreply.github.com>
2022-09-29 15:36:34 -0700

    Mv dev (#29)
    * remove navbar left margin
    
    * change nav li border-bottom size
    
    * move Navbar out of MainComponent.js into App.js
    
    * add dynamic links in navbar
    
    * added link for logo in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,33 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 import "./Navbar.css";
 
-const Navbar = () => {
+
+const Navbar = (props) => {
+  const location = useLocation()
+  let path = location.pathname === '/login' || location.pathname === '/passwordReset'
+  let home = '/'
   return (
     <div className="navbar">
       <div className="logo" alt="logo-title">
-        <span>MIXELLENCE</span>
+        <a href={path ? home : "#top"}>MIXELLENCE</a>
       </div>
 
       <nav className="nav">
         <ul>
           <li>
-            <a href="#about">About</a>
+            <a href={path ? home : "#about"}>About</a>
           </li>
           <li>
-            <a href="#drinks">Drinks</a>
+            <a href={path ? home : "#drinks"}>Drinks</a>
           </li>
           <li>
-            <a href="#bartenders">Bartenders</a>
+            <a href={path ? home : "#bartenders"}>Bartenders</a>
           </li>
           <li>
-            <a href="#testimonials">Testimonials</a>
+            <a href={path ? home : "#testimonials"}>Testimonials</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href={path ? home : "#contact"}>Contact</a>
           </li>
         </ul>
       </nav>
