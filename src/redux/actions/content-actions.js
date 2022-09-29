f8156f48ae1f07bf24a707e1252206877ad01807
mikegv <xmik3yvx@live.com>
2022-09-29 10:44:41 -0700

    formated code in content-actions

--- src/redux/actions/content-actions.js
+++ src/redux/actions/content-actions.js
@@ -1,32 +1,39 @@
-
 import { getDocs } from "firebase/firestore";
-import { aboutRef, barRef, drinksRef, testRef } from "../../firebase-db"
+import { aboutRef, barRef, drinksRef, testRef } from "../../firebase-db";
 import { contentActions } from "../slices/content-slice";
 
 //get firebase database data
 //
 export const firebaseContent = () => {
-    return async (dispatch) => {
-        //call startingload when going into function
-        //await getDocs()
-        //.then { on success dispatch stoppingLoad when everything fetched}
-        //.catch { error something dispatch stoppingLoad }
-        let aboutContent = [];
+  return async (dispatch) => {
+    //run getDocs on each collection
+    //then put that retrieved data into redux state
+    //
 
+    let aboutContent = [];
     await getDocs(aboutRef).then((res) => {
       res.docs.forEach((docs) => {
         aboutContent.push({ ...docs.data() });
-        dispatch(contentActions.updateContent({ property: 'aboutUs' , data: aboutContent[0].summary}))
-      })
+        dispatch(
+          contentActions.updateContent({
+            property: "aboutUs",
+            data: aboutContent[0].summary,
+          })
+        );
+      });
     });
 
     let bartenderContent = [];
-
     getDocs(barRef).then((res) => {
       res.docs.forEach((item) => {
         bartenderContent.push({ ...item.data(), id: item.id });
       });
-      dispatch(contentActions.updateContent({ property: 'bartenders' , data: bartenderContent}))
+      dispatch(
+        contentActions.updateContent({
+          property: "bartenders",
+          data: bartenderContent,
+        })
+      );
     });
 
     let drinksContent = [];
@@ -34,14 +41,25 @@ export const firebaseContent = () => {
       res.docs.forEach((item) => {
         drinksContent.push({ ...item.data(), id: item.id });
       });
-      dispatch(contentActions.updateContent({ property: 'drinks' , data: drinksContent}))
+      dispatch(
+        contentActions.updateContent({
+          property: "drinks",
+          data: drinksContent,
+        })
+      );
     });
+
     let testimonialsContent = [];
     getDocs(testRef).then((res) => {
       res.docs.forEach((item) => {
         testimonialsContent.push({ ...item.data(), id: item.id });
       });
-      dispatch(contentActions.updateContent({ property: 'testimonials' , data: testimonialsContent}))
+      dispatch(
+        contentActions.updateContent({
+          property: "testimonials",
+          data: testimonialsContent,
+        })
+      );
     });
-    }
-}
+  };
+};
