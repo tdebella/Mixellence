8e40e2022423047e300653a27a2a9bd28e5d469e
Mike <mikegv@users.noreply.github.com>
2022-09-30 10:17:38 -0700

    Mv dev (#32)
    * add login functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,21 +1,51 @@
 import React, { useState } from "react";
 import "./PasswordReset.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { StyledButton } from "../../styled-components/styled-components";
-
+import { useDispatch } from "react-redux";
+import firebaseConfig from "../../firebase-config";
+import { authActions } from "../../redux/slices/auth-slice";
 // social icons
 import Instagram from "../../assets/Photos/instagram.png";
 import Facebook from "../../assets/Photos/facebook.png";
 import Twitter from "../../assets/Photos/twitter.png";
 
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const apiKey = firebaseConfig.apiKey;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // console.log(email);
-    // console.log(password);
+    console.log('submit')
+    try {
+      fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            email,
+            password,
+            returnSecureToken: true
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+            //save token to local storage
+            //set redux state to loggedIn
+            //and redirect to dashboard page
+            //
+            localStorage.setItem('token', data.idToken)
+            dispatch(authActions.logIn())
+            navigate('/')
+        })
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const emailHandler = (e) => {
